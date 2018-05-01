18dca2ab0a479888d45ad318d54bfa9f0e4a2237
RisoKrizan <richard.krizan@outlook.com>
2018-05-02 00:10:22 +0200

    Absencie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501120317) do
+ActiveRecord::Schema.define(version: 20180501215003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180501120317) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "students_id"
+    t.string "reason"
     t.index ["students_id"], name: "index_absences_on_students_id"
   end
 
