49276538ba4af5f04a31966704eadc8c7f5bf5fe
Adrián Vyskoč <adrian.vyskoc@exponea.com>
2018-05-02 21:51:36 +0200

    exportovanie_db

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,22 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+$(document).ready(function() {
+    $("#btnExport").click(function(e) {
+        e.preventDefault();
+
+        //getting data from our table
+        var data_type = 'data:application/vnd.ms-excel';
+        var table_div = document.getElementById('tableWrapper');
+        var table_html = table_div.outerHTML.replace(/ /g, '%20');
+
+        var a = document.createElement('a');
+        a.href = data_type + ', ' + table_html;
+
+        var date = new Date();
+        var today = date.getDate() + "-" + (date.getMonth()+1) + "-" + (date.getFullYear());
+        a.download = "znamky_" + today + '.xls';
+        a.click();
+    });
+});
