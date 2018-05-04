0f75ac21d988d5e8a929eac49dc5e8cda5dc481e
RisoKrizan <richard.krizan@outlook.com>
2018-05-04 21:54:48 +0200

    zapis znamky

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,41 @@
 class TeachersController < ApplicationController
+  before_action :authenticate_teacher!, only: []
+
+  def show_teacher_grades
+    id_ucitela = params[:teach_id]
+    if (id_ucitela.to_i == current_teacher.id)
+      @all_grades=ActiveRecord::Base.connection.execute("
+      SELECT s2.name as stname ,c2.name as trname, s3.name as prname ,grades.value as znamka, grades.created_at as datum FROM grades
+        JOIN students s2 ON grades.students_id = s2.id
+        JOIN subjects s3 ON grades.subjects_id = s3.id
+        JOIN teacher_subjects s4 ON s3.id = s4.subjects_id
+        JOIN teachers t ON s4.teachers_id = t.id
+        JOIN classes c2 ON s2.classes_id = c2.id
+      WHERE (t.id=" + id_ucitela + ")
+")
+    end
+  end
+  def pridaj
+    @grade = Grade.new
+
+  end
+  def add_grade
+    @grade = Grade.new(grade_params)
+
+    respond_to do |format|
+      if @grade.save
+        format.html {redirect_to znamky_ucitela_path, notice: 'Známka úspešne pridaná'}
+        #format.json {render :show, status: :created, location: @grade}
+      else
+        format.html {render :pridaj}
+        format.json {render json: @grade.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+
+  private
+  def grade_params
+    params.require(:grade).permit(:value, :students_id, :subjects_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   root to: 'homes_#index'
   get "znamky/:stud_id", to: "students#show_all_grades"
   get "absencie/:stud_id", to: "students#show_all_absence"
-  get "znamky/:stud_id/export", to: "students#export_csv_xls_grades", as: "znamky"
+  get "zapis_znamky/:teach_id", to: "teachers#show_teacher_grades", as: "znamky_ucitela"
+  get "zapis_znamky/:teach_id/pridaj", to: "teachers#pridaj"
+  post "zapis_znamky/:teach_id/pridaj", to: "teachers#add_grade"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
