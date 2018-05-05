974a7681acb587585d6e95a5f87737739781e650
RisoKrizan <richard.krizan@outlook.com>
2018-05-05 02:54:35 +0200

    funguje to ako ma

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,3 +1,25 @@
 class GradesController < ApplicationController
 
+def show
+
+end
+
+  def update
+    @grade=Grade.find(params[:id])
+    respond_to do |format|
+      if @grade.update(grade_params)
+        format.html { redirect_to "/zapis_znamky/"+current_teacher.id.to_s+"", notice: 'Známka úspešne upravená' }
+        #format.json { render :show, status: :ok, location: @account }
+      else
+        format.html { render :edit }
+        format.json { render json: @grade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+  def grade_params
+    params.require(:grade).permit(:value, :students_id, :subjects_id)
+  end
 end
