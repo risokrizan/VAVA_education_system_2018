e2dd8bbab36765e567de50c7e52086bb878c125d
RisoKrizan <richard.krizan@outlook.com>
2018-05-05 03:12:51 +0200

    rozchodene grafy

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,12 @@ class HomesController < ApplicationController
     before_action :authenticate_teacher!, except: [ :index]
 
   def index
-    @pocet_ucitelov = ActiveRecord::Base.connection.execute("SELECT count(*) AS u_count FROM teachers;")
-    @pocet_studentov = ActiveRecord::Base.connection.execute("SELECT count(*) AS p_count FROM students;")
+    @pocet_ucitelov = Teacher.all.count
+    @pocet_studentov = Student.all.count
+    @pocet_jednotiek = Grade.all.where(value: 1).count
+    @pocet_dvojok = Grade.all.where(value: 2).count
+    @pocet_trojok = Grade.all.where(value: 3).count
+    @pocet_stvoriek = Grade.all.where(value: 4).count
+    @pocet_patiek = Grade.all.where(value: 5).count
   end
 end
